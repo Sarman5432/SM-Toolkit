d537a173ca1722ece7d444e04c63d4c70821af8a
Mahdi Rahman <35085896+mahdirahman1@users.noreply.github.com>
2020-08-20 00:48:57 -0400

    Error handling added

--- js/basic_calc.js
+++ js/basic_calc.js
@@ -1,6 +1,9 @@
 $("input[type = 'button']").on('click', function(){
-    $(this).addClass("clicked");
-    $(this).addClass("clicked");
+    $(this).addClass("clicked").delay(200).queue(function( next ){
+        $(this).removeClass('clicked'); 
+        next();
+    });
+    
 
     var value = $(this).val();
     if(value == 'x'){
@@ -10,7 +13,14 @@ $("input[type = 'button']").on('click', function(){
         $(".display").val($(".display").val() + '/'); 
     } 
     else if(value == "ENTER"){
-        $(".display").val(eval($(".display").val())); 
+        try{
+            $(".display").val(eval($(".display").val()));    
+        } catch(e){
+                $(".display").val("Math Error").delay(1500).queue(function( next ){
+                    $(this).val(''); 
+                    next();
+        });
+        } 
     } 
     else if(value == "AC"){
         $(".display").val("");
